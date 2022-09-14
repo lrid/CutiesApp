3ff0d8317afa8805ac221f1dcf49666f215acb2b
Laura <30292587+lrid@users.noreply.github.com>
2022-09-14 11:44:35 +0100

    created login screen and navigation

---
+++ src/screens/login.js
@@ -0,0 +1,28 @@
+import { View, StyleSheet, Text } from 'react-native'
+import { useState } from 'react'
+
+
+export default function LogIn () {
+    
+    return (
+        <View style={styles.container}>
+            <Text style={styles.headerText}>Welcome Back!</Text>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'pink',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText : {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+
+    },
+});
