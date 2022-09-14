a18f50d207e4997362db745cc5b6f712a8360c1e
Laura <30292587+lrid@users.noreply.github.com>
2022-09-14 17:51:40 +0100

    added firebase

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,14 +1,46 @@
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, KeyboardAvoidingView, TextInput } from 'react-native'
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 export default function LogIn () {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     
     return (
-        <View style={styles.container}>
-            <Text style={styles.headerText}>Welcome Back!</Text>
-
-        </View>
+        <KeyboardAvoidingView style={styles.container} behavior='padding'>
+            <View>
+                <Text style={styles.headerText}>Welcome Back!</Text>
+                <TextInput 
+                placeholder="Email"
+                value={email}
+                onChange= {text => setEmail(text)}
+                style={styles.input}
+                />
+                <TextInput 
+                placeholder="Password"
+                value={password}
+                onChange= {text => setPassword(text)}
+                style={styles.input} 
+                secureTextEntry
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                onPress={() => { }}
+                style={styles.buttonStyle}
+                >
+                    <Text style={styles.ButtonTextStyle}>Log In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => { }}
+                style={[styles.buttonStyle, styles.buttonOutline]}
+                >
+                    <Text style={styles.buttonOutlineText}>Register</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -23,6 +55,41 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 30,
         fontWeight: 'bold',
-
+        marginBottom:40
+    },
+    input : {
+        backgroundColor: 'white',
+        paddingHorizontal:15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginBottom: 10
+    },
+    buttonStyle:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+        backgroundColor: '#95D1A2',
+        height: 35,
+        width: 120,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 10,
+    },
+    ButtonTextStyle:{
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    buttonOutline :{
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: '#95D1A2',
+        borderWidth: 2
+    },
+    buttonOutlineText : {
+        color: '#95D1A2',
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        paddingTop: 30,
     },
 });
